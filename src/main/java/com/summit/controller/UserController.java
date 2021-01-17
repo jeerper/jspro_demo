f9be5474a83045144b4a078ba4b781ea7fd41028
jeerper <2997272453@qq.com>
2021-01-17 20:58:57 +0800

    跨域修改

--- src/main/java/com/summit/controller/UserController.java
+++ src/main/java/com/summit/controller/UserController.java
@@ -24,6 +24,7 @@ import java.util.Date;
  */
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
