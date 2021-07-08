5bb7e6a8492c867a3a327834a6f337e6a4ed0eb4
jeerper <297272453@qq.com>
2021-07-08 11:26:58 +0800

    加日志

--- src/test/java/com/summit/lambda/MapOrFlatMapTest.java
+++ src/test/java/com/summit/lambda/MapOrFlatMapTest.java
@@ -1,12 +1,13 @@
 package com.summit.lambda;
 
 import com.alibaba.fastjson.JSONObject;
+import com.summit.controller.UserController;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.util.Lists;
 import org.junit.Test;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -27,13 +28,14 @@ import static java.util.stream.Collectors.toList;
 @RunWith(SpringRunner.class)
 @Slf4j
 public class MapOrFlatMapTest {
+    private static final Logger logger = LoggerFactory.getLogger(MapOrFlatMapTest.class);
     @Test
     public void testMap(){
         ArrayList<String> words = Lists.newArrayList("java8", "Lambdas", "in");
         List<Integer> collect = words.stream().map(String::length).collect(toList());
-        log.info("words 长度是words:{}", ""+collect); //words 长度是:[5, 7, 2]
+        logger.info("words 长度是words:{}", ""+collect); //words 长度是:[5, 7, 2]
         Map<String, Integer> collect1 = words.stream().collect(Collectors.toMap(i -> i, i -> i.length()));
-        log.info("collect1 map是:{}", collect1);//{Lambdas=7, in=2, java8=5}
+        logger.info("collect1 map是:{}", collect1);//{Lambdas=7, in=2, java8=5}
     }
     @Test
     public void testMap2(){
@@ -42,7 +44,7 @@ public class MapOrFlatMapTest {
                 .map(word -> word.split(""))
                 .distinct()
                 .collect(toList());
-        log.info("collect2 集合是:{}", JSONObject.toJSONString(collect2));//collect2 集合是:[["h","e","l","l","o","w","o","r","l","d"]]
+        logger.info("collect2 集合是:{}", JSONObject.toJSONString(collect2));//collect2 集合是:[["h","e","l","l","o","w","o","r","l","d"]]
     }
     @Test
     public void testFlatMap(){
@@ -50,6 +52,6 @@ public class MapOrFlatMapTest {
         List<String> collect = words.stream().map(str -> str.split("")).//将字符串通过“ ”分割成数组
                 flatMap(Stream::of).//通过Stream.of将数组生成一个Stream，然后通过flatMap将多个Stream流对象合并成一个Stream流
                 distinct().collect(toList());//去重输出
-        log.info("collect 集合是:{}", JSONObject.toJSONString(collect));// collect 集合是:["h","e","l","o","w","r","d"]
+        logger.info("collect 集合是:{}", JSONObject.toJSONString(collect));// collect 集合是:["h","e","l","o","w","r","d"]
     }
 }
